338d07758a92d96a60397af12aaf34d7c033fce5
King Revno <marcellomak@gmail.com>
2020-12-05 13:05:33 -0600

    route express pages

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // import express to start sever from node.js 
 const express = require("express");
 const mysql = require("mysql");
+const path = require('path')
+
+const router = express.Router();
 
 // start server 
 const app = express();
@@ -22,10 +25,37 @@ db.connect((error) => {
     }
 })
 
-//request and responds 
-app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>");
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    //homepage login
 });
+
+//request and responds the html links 
+app.get('/index.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    //homepage login
+});
+
+app.get('/sign_up.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/sign_up.html'));
+    //signup page
+})
+
+app.get('/admin_home.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/admin_home.html'));
+    //admin home page
+})
+
+app.get('/superadmin_page.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/superadmin_page.html'));
+    //superadmin home page
+})
+
+app.get('/user_home.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/user_home.html'));
+    //user home page
+})
+
 
 //express listen for port 3020
 app.listen(3020, () => {
