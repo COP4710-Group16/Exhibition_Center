d1451194b3dc72e2c7a1c94c53e5a57afd9f356b
unknown <AlexanderALongo@knights.ucf.edu>
2020-12-06 15:43:00 -0500

    Fixed up the routing so the server can serve the pages and API

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // import express to start sever from node.js 
 const express = require("express");
-const mysql = require("mysql");
 const path = require('path')
-const dotenv = require('dotenv')
-const router = express.Router();
-
-
-// obtain .env with sensitive data  
-dotenv.config({ path: './.env' })
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+const port = process.env.PORT || 3020;
+var router = express.Router();
+require('dotenv').config();
 
 // start server 
 const app = express();
+app.set('port', 3020);
+app.use(express.json());
+
+// Rest API requirements
+app.use(bodyParser.urlencoded({
+	extended: true
+  }));
+  app.use(bodyParser.json());
 
 //database is being pulled from phpmyAdmin
 const db = mysql.createConnection({
@@ -19,8 +25,6 @@ const db = mysql.createConnection({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
 })
-
-// app.set('view engine', 'html');
 
 //check if we connected 
 db.connect((error) => {
@@ -31,16 +35,53 @@ db.connect((error) => {
     }
 })
 
-
+/*
 // obtain public html files to render
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
+*/
 
-app.use('/', require('./routes/pages'));
+/*
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, './public')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+*/
 
-//express listen for port 3020
-app.listen(3020, () => {
-    console.log("Server started correctly");
+/*
+if (process.env.NODE_ENV === 'production')
+{
+    app.use(express.static(__dirname));
+    app.use(express.static(path.join(__dirname, './public')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './public', 'index.html'));
+    });
+}
+*/
+
+// Serves the pages
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+// Just a test endpoint
+app.get('/user', (req, res) => {
+	res.send("Helloo")
 })
 
+//app.get('/', require('./routes/pages'));
+
+// Serves the api
+app.use('/api', require('./routes/api'));
+
+
+//express listen for port
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
