30748a1236a5d75309f2f0519cee32b640907c5a
Cameron Lindsey <cameronlindsey@knights.ucf.edu>
2020-12-08 18:12:55 -0500

    -Hooked up super admin front end with api

---
+++ public/admin.js
@@ -0,0 +1,86 @@
+function adminSearch()
+{
+    var name = document.getElementById("adminSearchName").value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            adminSearchCallback(xmlHttp.response);
+    }
+    xmlHttp.responseType = 'json';
+    xmlHttp.open( "GET", "/api/getEventsByAdmin?adminName="+name, true ); // false for synchronous request
+    xmlHttp.send( null );
+}
+
+function adminSearchCallback(jsonData)
+{
+    var table = document.getElementById("adminSearchEvents");
+
+    for(var i = table.rows.length - 1; i > 0; i--)
+    {
+        table.deleteRow(i);
+    }
+
+    data = jsonData;
+    jsonData.forEach((event, index) =>
+    {
+        insertAdminRow(event.username, event.eventTitle, event.eventURL, event.eventStartDate, event.eventEndDate, event.city);
+    });
+}
+
+function insertAdminRow(username, title, url, start, end, city)
+{
+    var table = document.getElementById("adminSearchEvents");
+    var row = table.insertRow();
+
+    var userCell = row.insertCell(0);
+    var titleCell = row.insertCell(1);
+    var urlCell = row.insertCell(2);
+    var startCell = row.insertCell(3);
+    var endCell = row.insertCell(4);
+    var cityCell = row.insertCell(5);
+
+    userCell.innerHTML = username;
+    titleCell.innerHTML = title;
+    urlCell.innerHTML = url;
+    startCell.innerHTML = start;
+    endCell.innerHTML = end;
+    cityCell.innerHTML = city;
+}
+
+function userSearch()
+{
+    var name = document.getElementById("userSearchName").value;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        userSearchCallback(xmlHttp.response);
+    }
+    xmlHttp.responseType = 'json';
+    xmlHttp.open( "GET", "/api/getEventsByAdmin?adminName="+name, true ); // false for synchronous request
+    xmlHttp.send( null );
+}
+
+function userSearchCallback(jsonData)
+{
+    var table = document.getElementById("userSearchEvents");
+
+    for(var i = table.rows.length - 1; i > 0; i--)
+    {
+        table.deleteRow(i);
+    }
+
+    data = jsonData;
+    jsonData.forEach((event, index) =>
+    {
+        insertUserRow(event.eventTitle);
+    });
+}
+
+function insertUserRow(title)
+{
+    var table = document.getElementById("userSearchEvents");
+    var row = table.insertRow();
+
+    var titleCell = row.insertCell(0);
+    titleCell.innerHTML = title;
+}
